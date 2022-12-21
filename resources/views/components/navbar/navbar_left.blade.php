78309b0b93029d1f4c83120f74795b49f419a414
Alessandro Geras <alessandrogeras@gmail.com>
2022-12-21 01:55:26 -0300

    first commit

--- resources/views/components/navbar/navbar_left.blade.php
+++ resources/views/components/navbar/navbar_left.blade.php
@@ -1,3 +1,6 @@
-<div class="bg-[#06103d] h-10 w-full">
-
+<div class="ml-[62px] flex text-white items-center h-10 text-xs font-semibold">
+    <div class="p-3 hover:bg-[#2969f2] cursor-pointer">About FlightForce</div>
+    <div class="p-3 hover:bg-[#2969f2] cursor-pointer">Civil Aviation</div>
+    <div class="p-3 hover:bg-[#2969f2] cursor-pointer">Defense & Security</div>
+    <div class="p-3 hover:bg-[#2969f2] cursor-pointer">SRT Gay</div>
 </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,26 +5,30 @@ date_default_timezone_set('America/Sao_Paulo');
 @endphp
 
 @if(session('msg'))
-  <script>alert("{{session('msg')}}")</script>
+<script>
+    alert("{{session('msg')}}")
+</script>
 @endif
-  
+
 <html>
 
 <head>
-  <title>@yield("title")</title>
+    <title>@yield("title")</title>
 
-  <x-layout.head />
+    <x-layout.head />
 </head>
 
 <body>
 
-  <navbar>
-    <x-navbar.navbar_left />  
-  </navbar>  
+    <navbar>
+        <div class="bg-[#06103d] h-10 w-full">
+            <x-navbar.navbar_left />
+        </div>
+    </navbar>
 
-  <main>
-    @yield("corpo")
-  </main>
+    <main>
+        @yield("corpo")
+    </main>
 
 </body>
 
